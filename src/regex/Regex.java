374e93a5693173a41225f2fbfe8b369cc5be3794
MuthuKumarB82 <113657584+MuthuKumarB82@users.noreply.github.com>
2023-06-26 11:16:48 +0530

    Update Regex.java

--- src/regex/Regex.java
+++ src/regex/Regex.java
@@ -2,7 +2,7 @@ package regex;
 
 public class Regex {
 	public static void main(String[] args) {
-		String a = "Mut786H78U00kum54236ar898B";
+		String a = "Mut786H78U00kum54236ar898BCC";
 //		String[] b = a.split("\\d+");
 //		for(int i=0;i<b.length;i++) {
 //			System.out.print(b[i]);
